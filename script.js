c02ccb0e6b7a8fd4aafbc3987caa324a5dd602b3
Stanley Odhiambomwai <odhiambostanley799@gmail.com>
2024-11-15 20:07:21 +0300

    function with conditions

--- script.js
+++ script.js
@@ -3,3 +3,10 @@
 //Leap years are divisible by 4, but not by 100 
 //unless they are also divisible by 400.
 
+function isLeapYear(year) {
+    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
