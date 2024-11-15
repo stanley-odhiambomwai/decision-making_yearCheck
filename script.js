14dc69103180be6154209cf0c0da6d62850c6266
Stanley Odhiambomwai <odhiambostanley799@gmail.com>
2024-11-15 20:04:05 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+//Create a function that takes a year as input and 
+//determines whether it is a leap year or not. 
+//Leap years are divisible by 4, but not by 100 
+//unless they are also divisible by 400.
+
